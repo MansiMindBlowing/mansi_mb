6c8564f2d8b3387293fc80d94d0d1c87c3a0d46c
mansi kuraria <mansi.kuraria@mindbowser.com>
2025-06-24 21:19:13 +0530

    day 08 commit

---
+++ node/week2/day8/tempalte enginee/routes/admin.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+const express = require('express');
+
+const productsController = require('../controllers/products')
+const rootDir = require('../util/path');
+
+const router = express.Router();
+
+const products = [];
+
+router.get('/add-product',productsController.getAddProduct);
+
+// /admin/add-product => POST
+router.post('/add-product',productsController.postAddProduct );
+;
+
+// exports.routes = router;
+// exports.products = products;
+module.exports = router;
